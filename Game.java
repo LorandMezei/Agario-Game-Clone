3e305b0995d016ea33a2e77e436e65311002b15d
Lorand Mezei <lorand_mezei@yahoo.com>
2021-01-18 20:14:25 -0500

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,19 @@
+package game;
+
+public class Game 
+{       
+    /*
+        Use a timer to smooth out direction changes. 
+        If the x changes little and y more, make the timer delay slower and change only x,
+                                            make the timer delay faster and change only y.
+                                            So maybe make a timer for x changing,
+                                            and a timer for y changing.
+    
+    */
+    
+    public static void main(String[] args)  throws Exception
+    {
+        new GameFrame();   
+    }   //----------------------   End Main Method   ----------------------//    
+    
+}   //-------------------------   End Public Class   -------------------------//
